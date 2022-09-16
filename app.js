48874ebcb26bdc0214a9572989232838a146c0f6
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2022-09-17 01:50:49 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,93 +1,137 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-const items = ["Milk", "Bread", "Eggs"];
-const workItems = [];
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
 
-
-// Telling express to use public folder for css.
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-// boiler code from ejs for express
-app.set("view engine", "ejs");
-//sending data to html
-app.get("/", function (req, res) {
 
-	const day = date.getDate();
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-	// render is used for ejs to change the data
-	res.render("list", { listTitle: day, newListItems: items });
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
 });
 
-// Cating the data after pressing submit
-app.post("/", function (req, res) {
-
-	const item = req.body.newItem;
-	if (req.body.list === "Work") {
-		workItems.push(item);
-		res.redirect("/work");
-	} else {
-		items.push(item);
-		res.redirect("/");
-	}
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
 });
 
-// Targeting work route
-app.get("/work", function (req, res) {
-	res.render("list", {listTitle: "Work List", newListItems: workItems});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems){
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully savevd default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
+
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        //Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        //Show an existing list
+
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === "Today"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (!err) {
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
+
 });
 
 app.get("/about", function(req, res){
-	 res.render("about");
+  res.render("about");
 });
 
-//sever is staring for project node/nodemon
-app.listen(process.env.PORT || 3000, function (req, res) {
-	console.log("Sever is started on port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
-
-
-
-
-
-
-
-// Alternative Ways to get a week day!
-
-// let date = new Date();
-//     let weekDays =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-//     let day = date.getDay();
-
-//     res.render("list",{kindOfDay: weekDays[day]})
-
-// Other Way!
-
-// var currentDay = today.getDay();
-//   var day = "";
-
-//   switch (currentDay) {
-//     case 0:
-//       day = "Sunday";
-//       break;
-//     case 1:
-//       day = "Tuesday";
-//       break;
-//     case 3:
-//       day = "Wednesday";
-//       break;
-//     case 4:
-//       day = "Thursday";
-//       break;
-//     case 5:
-//       day = "Friday";
-//       break;
-//     case 6:
-//       day = "Saturday";
-//       break;
-
-//     default:
-//     console.log("Error: current day is equal to: " + currentDay);
-//   }
