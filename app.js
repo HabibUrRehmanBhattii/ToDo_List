d49e6a1b67b56e98944361fdac9a1a27d819ff26
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2022-09-17 06:33:43 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-learn:"PASSWORD COME HERE"@cluster0.oqtggvy.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -132,6 +133,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if(port === null || port === ""){
+  port = 3000;
+}
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on Sussfully");
 });
